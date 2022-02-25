9dd0bd918183fdd78062abb89d04a6aad435aecd
Luis Carlos <luiscarlosnors@gmail.com>
2022-02-24 23:28:29 -0300

    generators

---
+++ 04 - Código Assíncrono/03 - Generators/main.js
@@ -0,0 +1,20 @@
+function* myCounter() {
+  var i = 0;
+  while (true) {
+    yield i++;
+  }
+}
+
+var counter = myCounter();
+var counterValue = counter.next();
+
+console.log(counterValue);
+
+function* myGenerator() {
+  yield* [1, 2, 3, 4, 5];
+}
+
+var myGen = myGenerator();
+console.log(myGen.next());
+console.log(myGen.next());
+console.log(myGen.next());
