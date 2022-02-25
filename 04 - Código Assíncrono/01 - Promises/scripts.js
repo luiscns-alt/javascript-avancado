42daaea57dfe1d6ba139a84973855f8df12f0f14
Luis Carlos <luiscarlosnors@gmail.com>
2022-02-24 23:02:05 -0300

    promises

---
+++ 04 - Código Assíncrono/01 - Promises/scripts.js
@@ -0,0 +1,27 @@
+var myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(500);
+  }, 2000);
+});
+
+myPromise
+  .then((value) => {
+    console.log(`My Value: ${value}`);
+    return 900;
+  })
+  .then((value) => {
+    console.log(`My Value: ${value}`);
+    return 100;
+  });
+
+var p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(500);
+  }, 2000);
+});
+
+var p2 = Promise.resolve(77);
+
+Promise.all([p1, p2]).then((value) => {
+  console.log(value);
+});
