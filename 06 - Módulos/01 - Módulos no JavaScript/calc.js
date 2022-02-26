f9531d1a28ceb71f76b41395981d04b63dde31fb
Luis Carlos <luiscarlosnors@gmail.com>
2022-02-26 10:32:10 -0300

    módulos

---
+++ 06 - Módulos/01 - Módulos no JavaScript/calc.js
@@ -0,0 +1,12 @@
+export var myVar = 5;
+var myVar2 = 10;
+
+export function myFunc() {
+  console.log("TreinaWeb");
+}
+
+export default class Calc {
+  static sum(a, b) {
+    return a + b;
+  }
+}
