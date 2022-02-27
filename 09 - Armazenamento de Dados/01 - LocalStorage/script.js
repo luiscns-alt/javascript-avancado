c6644b9eddfc2cc834e9305443898c79c49697a4
Luis Carlos <luiscarlosnors@gmail.com>
2022-02-27 19:55:16 -0300

    localstorage

---
+++ 09 - Armazenamento de Dados/01 - LocalStorage/script.js
@@ -0,0 +1,20 @@
+localStorage;
+
+localStorage.setItem("nome", "Luis");
+
+localStorage.getItem9("nome");
+
+let myObj = { nome: "luis" };
+
+console.log(localStorage.setItem("nome", myObj));
+console.log(localStorage.setItem("nome", JSON.stringify(myObj)));
+
+localStorage.getItem("nome");
+var myobjl = localStorage.getItem("nome");
+JSON.parse(myobjl);
+
+localStorage.nome;
+localStorage["nome"];
+
+localStorage.removeItem("nome");
+localStorage.clear();
